450e4f13ebd4a9f60a1da090f0330d28f17bd5f0
rg2570 <rg2570>
2020-11-23 11:45:41 +0330

    producer 1st impl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
-import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.*;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
@@ -7,11 +11,31 @@ import org.apache.kafka.streams.kstream.KStream;
 
 import java.util.Properties;
 
-public class Main {
-    public static void main(String[] args) throws  Exception{
-        Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "first-sample");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "172.30.5.235:9092");
+public class Main
+{
+    public static void main(String[] args) throws  Exception
+    {
+        final  String TOPIC = "k2-topic";
+        final  String BOOTSTRAP_SERVERS = "172.30.5.235:9092,172.30.5.236:9092,172.30.5.236:9092";
+            Properties props = new Properties();
+            props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+            props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC, "key2", "val2");
+        try {
+            producer.send(record);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
 
     }
+
+
+        
+
 }
