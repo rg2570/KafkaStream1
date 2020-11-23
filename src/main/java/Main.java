3bdbc65eb5443cbf3783e4419f2fe8a94ce8a8e1
rg2570 <rg2570>
2020-11-23 15:32:59 +0330

    new codes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.protocol.types.Field;
 import org.apache.kafka.common.serialization.*;
 import org.apache.kafka.streams.KafkaStreams;
@@ -8,34 +6,53 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class Main
 {
     public static void main(String[] args) throws  Exception
     {
-        final  String TOPIC = "k2-topic";
-        final  String BOOTSTRAP_SERVERS = "172.30.5.235:9092,172.30.5.236:9092,172.30.5.236:9092";
+        final  String TOPIC = "k1-topic";
+        final  String BOOTSTRAP_SERVERS = "172.30.5.235:9092,172.30.5.236:9092,172.30.5.237:9092";
             Properties props = new Properties();
             props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-            props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-            props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            props.put(StreamsConfig.APPLICATION_ID_CONFIG, "bigd");
+            props.put("key.serializer", StringSerializer.class.getName());
+            props.put("value.serializer", StringSerializer.class.getName());
 
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
 
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC, "key2", "val2");
-        try {
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("bigd", "bigd", "Another one");
+        try
+        {
+            /*
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    Logger logger= (Logger) LoggerFactory.getLogger(Main.class);
+                    if (e== null) {
+                        logger.info("Successfully received the details as: \n" +
+                                "Topic:" + recordMetadata.topic() + "\n" +
+                                "Partition:" + recordMetadata.partition() + "\n" +
+                                "Offset" + recordMetadata.offset() + "\n" +
+                                "Timestamp" + recordMetadata.timestamp());
+                    }
+
+                    else {
+                        logger.warning(("Can't produce,getting error"));
+
+                    }
+                }
+            });
+             */
             producer.send(record);
-        }catch (Exception e){
+            producer.flush();
+            producer.close();
+        }catch (Exception e)
+        {
             e.printStackTrace();
         }
-
-
-
     }
-
-
-        
-
 }
